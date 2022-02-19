9810699ecaaf7a1a7e3674babfd1d560dcc3ffb8
epeppin <epeppin@my.wctc.edu>
2022-02-19 17:48:59 -0600

    Update Changes for format

--- Program.cs
+++ Program.cs
@@ -6,38 +6,39 @@ namespace A3_Interpolation
     {
         static void Main(string[] args)
         {
-                DateTime today = DateTime.Now;
+                var date = DateTime.Now;
+                var dateFormatted = date.ToString("MMMM dd, yyyy");
                 //1.January 22, 2019
-                Console.WriteLine($"1.{today:MMMM} {today:dd}, {today:yyyy}");
+                Console.WriteLine($"1.{dateFormatted,40}");
 
                 //2.2019.01.22
-                Console.WriteLine($"2.{today:yyyy}.{today:MM}.{today:dd}");
+                Console.WriteLine($"2.{date:yyyy}.{date:MM}.{date:dd}");
 
                 //3.Day 22 of January, 2019
-                Console.WriteLine($"3.Day {today:dd} of {today:MMMM}, {today:yyyy}");
+                Console.WriteLine($"3.Day {date:dd} of {date:MMMM}, {date:yyyy}");
 
                 //4.Year: 2019, Month: 01, Day: 22
-                Console.WriteLine($"4.Year:{today:yyyy}, Month:{today:MM}, Day:{today:dd}");
+                Console.WriteLine($"4.Year: {date:yyyy}, Month: {date:MM}, Day: {date:dd}");
 
                 //5.            Tuesday (10 spaces)
-                Console.WriteLine($"5.{today, 10:dddd}");
+                Console.WriteLine($"5.{date, 10:dddd}");
 
                 //6.     11:01 PM             Tuesday (10 spaces between)
-                Console.WriteLine($"6.{today, 10:t}{today, 10:dddd}");
+                Console.WriteLine($"6.{date, 10:t}{date, 10:dddd}");
 
                 //7.h:11, m:01, s:27
-                Console.WriteLine($"7.h:{today:hh},m:{today:mm},s:{today:ss}");
+                Console.WriteLine($"7.h:{date:hh}, m:{date:mm}, s:{date:ss}");
 
                 //8.2019.01.22.11.01.27
-                Console.WriteLine($"8.{today:yyyy}.{today:MM}.{today:dd}.{today:hh}.{today:mm}.{today:ss}");
+                Console.WriteLine($"8.{date:yyyy}.{date:MM}.{date:dd}.{date:hh}.{date:mm}.{date:ss}");
                     
                 Console.WriteLine("------------------------------------------");
                 var pi = Math.PI;
                 //1. Output as currency
-                Console.WriteLine("1.{0:c2}", pi);
+                Console.WriteLine($"1.{pi:c2}");
 
                 //2. Output as right-aligned (10 spaces), number with 3 decimal places
-                Console.WriteLine("2.{0, 10:n3}", pi); 
+                Console.WriteLine($"2.{pi, 10:n3}"); 
             
 
             }
